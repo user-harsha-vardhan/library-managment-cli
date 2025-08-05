21058845f190c86b723975a6011ff12e1fc4e1d0
Harshavardhan <harshagowdaout987@gmail.com>
2025-08-05 12:07:23 +0530

    50% project completed till now

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+data class Book(
+    val id: Int,
+    val title: String,
+    val author: String,
+    var isAvailable: Boolean = true
+)
+fun main () {
+    val library = Library()
+    while (true) {
+        println(
+            """
+            ===== Library Menu ====
+            1.add book
+            2.view books
+            3.search book by title
+            4.search book by author
+            5.borrow book
+            6.return book
+            7.delete book 
+            8.exit
+        """.trimIndent()
+        )
+
+        println("\n enter your choice:")
+        when (readln().toIntOrNull()) {
+            1 -> {
+                println("enter title:")
+                val title = readln()
+                println("enter author:")
+                val author = readln()
+                library.addBook(title,author)
+            }
+
+            2 -> {
+                library.viewBooks()
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/classes.kt
@@ -0,0 +1,54 @@
+class Library {
+    private val books = mutableListOf<Book>()
+    private var id = 1
+
+    fun addBook(title: String, author: String) {
+        books.add(Book(id++, title, author))
+        println("\n book added")
+    }
+
+    fun viewBooks () {
+        if (books.isEmpty()) println("no books found")
+        else books.forEach { println(it)}
+    }
+
+    fun searchByTitle (title: String) {
+        val found = books.filter { it.title.contains(title, ignoreCase = true) }
+        if (found.isEmpty()) println("no book found by that title")
+        else books.forEach { println(it) }
+    }
+
+    fun searchByAuthor (author: String) {
+        val found  = books.filter {it.author.contains(author, ignoreCase = true)}
+        if (found.isEmpty()) println("no book found by that author name")
+        else books.forEach { println(it) }
+    }
+
+    fun borrowBook (id: Int){
+        val book = books.find { it.id == id }
+        if (book == null) println("book not found")
+        else if (!book.isAvailable) println("book is already borrowed")
+        else {
+            book.isAvailable = false
+            println("you borrowed ${book.title}")
+        }
+    }
+
+    fun returnBook (id: Int) {
+        val book = books.find{ it.id == id}
+        if (book == null) println("book not found")
+        else if (book.isAvailable) println("book was not borrowed")
+        else {
+            book.isAvailable = true
+            println("you returned: ${book.title}")
+        }
+    }
+
+
+    fun deleteBook (id: Int) {
+        if (books.removeIf { it.id == id }) println("book deleted")
+        else println("book not found")
+    }
+
+
+}
